bbf2f242b49ed90127e96e1df65547cabbbf0242
Eddie Franco <eddiefranco626@gmail.com>
2018-11-17 17:46:51 -0600

    Burger returning list items to the main page

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,6 +3,14 @@ const burger = require('../models/burger.js');
 
 const router = express.Router();
 
-
+router.get("/", function(req, res){
+    burger.selectAll(function(data){
+        let hbsObj = {
+            burger: data
+        };
+        console.log(hbsObj);
+        res.render("index", hbsObj);
+    })
+})
 
 module.exports = router;
