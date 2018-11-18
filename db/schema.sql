0bc32cd3829e1aa95f6228b26bedc1abbbe75006
Eddie Franco <eddiefranco626@gmail.com>
2018-11-17 21:08:43 -0600

    schema update

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,7 @@ CREATE TABLE burgers (
   devoured BOOLEAN NOT NULL,
   PRIMARY KEY(id)
 );
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Swiss Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black Bean Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Grilled Chicken Sandwich", FALSE);
